786d55e06e6f1a429d519e1f3ce81c9fcd9742d2
Damon Wang <damon.wang@gmail.com>
2010-07-15 15:43:28 -0500

    moved things around so that the working copy could be used as the source for imports. Now if you put the working copy's path into PYTHONPATH env. variable, calling "import larch" will get you the working copy instead of the site-wide production copy. Closes gh-7.

--- setup.py
+++ setup.py
@@ -11,12 +11,11 @@ setup(
     author_email = 'newville@cars.uchicago.edu',
     license = 'Python',
     description = 'A data processing macro language for python',
-    package_dir = {'larch': 'lib',
-                   'larch.modules':'modules',
+    package_dir = {'larch.modules':'modules',
                    'larch.plugins':'plugins',
                    },
     packages = ['larch','larch.plugins','larch.modules',
                 'larch.wx', 'larch.wx.mplot'],
     package_data = {'larch.modules':['startup.lar']},
-    data_files  = [('bin',['larch', 'wxlarch'])],)
+    data_files  = [('bin',['bin/larch', 'bin/wxlarch'])],)
 
